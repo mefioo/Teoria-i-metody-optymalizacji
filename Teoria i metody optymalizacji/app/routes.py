7e17fdaad9395d36d80ef21a6bf6c1e856f64e75
mefioo <38735691+mefioo@users.noreply.github.com>
2020-03-25 14:44:35 +0100

    Add files via upload

--- Teoria i metody optymalizacji/app/routes.py
+++ Teoria i metody optymalizacji/app/routes.py
@@ -1,11 +1,18 @@
 from app import app
-from flask import render_template
+from flask import flash, url_for, redirect, render_template
+from app.forms import InputData
 
 
 
 
 
-@app.route('/')
-@app.route('/main')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/main', methods=['GET', 'POST'])
 def main():
-    return render_template('main.html', title='Strona główna projektu')
+    form = InputData()
+    if form.validate_on_submit():
+        flash('Pomyślnie wykonano obliczenia!', 'success')
+        return redirect(url_for('main'))
+    else:
+        flash('Nie wykonano obliczeń')
+    return render_template('main.html', form=form, title='Strona główna projektu')
